f2a75201c92c05dbadc80c4e523b523b601b19dd
Jacob Cappi <jcappi0199@gmail.com>
2022-04-17 18:09:51 -0500

    Stable: got jumping

--- config.def.h
+++ config.def.h
@@ -206,6 +206,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,               Button4,        kscrollup,      {.i = 1} },
 	{ MODKEY,               Button5,        kscrolldown,    {.i = 1} },
+	{ TERMMOD,              XK_U,           kscrollup,      {.i = -1} },
+	{ TERMMOD,              XK_D,           kscrolldown,    {.i = -1} },
 	{ MODKEY,               XK_u,           kscrollup,      {.i = 1} },
 	{ MODKEY,               XK_d,           kscrolldown,    {.i = 1} },
 };
